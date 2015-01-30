013dd3391d3774319b8b79b2773f2ea3a363bfda
matthias <pokr4tes@gmail.com>
2015-01-30 23:07:05 +0100

    implemented basic app flow with alerts

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,21 +2,62 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
 
+	//TODO: Init Function
+	//TODO: callback function dry / self - this rework
+
 	counter: 0,
+
+	wrongQuestions : [],
 
   	question: function() {
     	return this.get('model').objectAt(this.counter);
   	}.property('model'),
 
-
   	actions: {
-  		nextQuestion: function() {
-  			console.log(this.get('model').length );
-  			if(this.counter < this.get('model').length ) {
-	  			this.counter++;
-	  			this.set('question', this.get('model').objectAt(this.counter));
-	  			console.log(this.counter);
-  			}
+
+  		//Called when user clicks true / false
+  		answerQuestion: function(answer) {
+
+  			//Callback for Alert - shows next Question after alert's closed
+  			var controller = this;
+  			var nextQuestion = function(){
+  				if(controller.counter < controller.get('model').get('length')-1){
+					controller.counter++;
+					controller.set(
+						'question', 
+						controller.get('model').objectAt(controller.counter)
+					);
+				}
+				else{
+					//TODO:
+					/*
+						Show End Screen:
+							Score
+							Again?
+								all / wrong
+					*/
+				}
+			}
+
+			//Handle User Answer - shows success/error alert
+			if(this.get('question').get('answer') === answer){
+				sweetAlert({
+					title: 'Korrekt...',
+					type: 'success',    
+					confirmButtonText: 'Nächste Frage',
+				}, nextQuestion);
+			}
+			else{
+				this.wrongQuestions.pushObject(this.get('question'));
+				sweetAlert({
+					title: 'Inkorrekt...',
+					type: 'error',    
+					confirmButtonText: 'Nächste Frage',
+				}, nextQuestion);
+			}
+
   		}
-  	}
+  	},
+
+
 });
