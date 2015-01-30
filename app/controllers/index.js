7d2d517aa21c5630c09dfa0d2b7aa0e5cccfc16c
matthias <pokr4tes@gmail.com>
2015-01-30 16:20:16 +0100

    minor changes index template / index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-	questionAt : 0,
-  	nextQuestion: function() {
-  		var question = this.get('model').objectAt(this.questionAt);
-  		this.questionAt+=1;
-    	return question;
-  	}.property('model')
+
+	counter: 0,
+
+  	question: function() {
+    	return this.get('model').objectAt(this.counter);
+  	}.property('model'),
+
+  	
+  	actions: {
+  		nextQuestion: function() {
+  			this.counter++;
+  			console.log(this.counter);
+  		}
+  	}
 });
