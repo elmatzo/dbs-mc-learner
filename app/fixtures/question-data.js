6e42459d9d7ea2b15e14a162a16c3003f8215883
matthias <pokr4tes@gmail.com>
2015-02-01 12:36:31 +0100

    added some questions

--- app/fixtures/question-data.js
+++ app/fixtures/question-data.js
@@ -2,16 +2,37 @@ export default [
     {
       id: 1,
       text: 'Bei der Bearbeitung der Anfrage σA>aR fuehrt ein Hash Index fuer das Attribut A manchmal zu einer effizienteren Auswertung aber niemals zu einer weniger effizienten Auswertung als ein B+ Baum Index fuer dieses Attribut.',
-      answer: true
+      answer: false
     },
     {
       id: 2,
-      text: 'bla',
+      text: 'Betrachten Sie die Kostenformel 2 · bR ·(1 + I) mit I = [logm−1 ([bR/m])] fuer das externe Sortieren. Dabei steht I fuer die Anzahl der “runs”.',
       answer: false
     },
     {
       id: 3,
-      text: 'Blub',
+      text: 'Die Historie r1(C), r2(D), w2(D), w1(D), w2(C), c1, c2 vermeidet kaskadierendes Ruecksetzen und ist nicht serialisierbar.',
+      answer: true
+    },
+    {
+      id: 4,
+      text: 'Ein geballter Index eignet sich vor allem bei Bereichsabfragen.',
+      answer: true
+    },
+    {
+      id: 5,
+      text: 'Betrachten Sie zwei Relationen R(ABC) und S(ABD). Dann gelten auf jeden Fall folgende Gleichheiten: (R SemiJoin S) = (R Join πA(S)) = (R Join πAB(S))',
       answer: false
-    }
+    },
+    {
+      id: 6,
+      text: 'Eine Relation R sei an 6 Netzwerk-Knoten materialisiert mit den Gewichten 5, 4, 3, 7, 2, und 9. Dann sind Qr(R) = 11 und Qw(R) = 19 gultige Lese- bzw. Schreib-Quoren.',
+      answer: false
+    },
+    {
+      id: 7,
+      text: 'Es gibt Relationen R(AB) mit 100 Tupeln und S(AC) mit 100 Tupeln, fuer die der Ausdruck R NaturalJoin S 10000 Tupeln ergibt.',
+      answer: false
+    },
+
 ];
